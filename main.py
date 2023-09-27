bd772e6a006820e652ab356374eb30a59f40e689
OverwatchUG <shadiduwaako@gmail.com>
2023-09-27 18:44:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.",
+#     "Function": "A piece of code that you can easily call over and over again.",
+# }
+#
+# print(programming_dictionary["Bug"])
+#
+# # adding new items to dictionary
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+#
+# print(programming_dictionary)
+
+# student_scores = {
+#     "Harry": 81,
+#     "Ron": 78,
+#     "Hermione": 99,
+#     "Draco": 74,
+#     "Nevile": 62,
+# }
+#
+# student_grades = {}
+#
+# for student in student_scores:
+#     score = student_scores[student]
+#     if score > 90:
+#         student_grades[student] = "Outstanding"
+#     elif score > 80:
+#         student_grades[student] = "Exceeds expectations"
+#     elif score > 70:
+#         student_grades[student] = "Acceptable"
+#     else:
+#         student_grades[student] = "Fail"
+#
+# print(student_grades)
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# nesting a dictionary in a dictionary
+#
+# travel_log = {
+#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
+
+# nesting a dictionary in a list
+
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "total_visits": 12
+#     },
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#          "total_visits": 5
+#      },
+# ]
+
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+     },
+]
+
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
